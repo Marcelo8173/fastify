52a9152cdfa6c1c5be8b67b32a620b41e23ec7f6
Marcelo8173 <marceloandrebio@gmail.com>
2023-02-06 15:56:26 -0300

    first commit

---
+++ db/migrations/20230206183731_create-transactions.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('transactions', (table) => {
+    table.uuid('id').primary()
+    table.text('title').notNullable()
+    table.decimal('amount', 10, 2).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('transactions')
+}
+
+// npm run knex -- migrate:latest npm run knex -- migrate:make create-documents
+// table.decimal('amount', 10, 2).notNullable() - precisão e scala
