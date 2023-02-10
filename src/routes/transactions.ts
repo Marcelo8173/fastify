66cc076746973d0a2a27383c5a90c6f0db6db913
Marcelo8173 <marceloandrebio@gmail.com>
2023-02-10 11:17:34 -0300

    create middleware

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { z as Zod } from 'zod'
 import { knex } from '../database'
 import { randomUUID } from 'crypto'
+import { checkIdExist } from '../middleware/check-session-id-exist'
 
 export async function transactionRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
@@ -12,49 +13,91 @@ export async function transactionRoutes(app: FastifyInstance) {
     })
 
     const { amount, title, type } = createBodySchema.parse(request.body)
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 24 * 7, // 7 days
+      })
+    }
 
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
   })
 
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  app.get(
+    '/',
+    {
+      preHandler: [checkIdExist],
+    },
+    async (request, reply) => {
+      const sessionId = request.cookies.sessionId
 
-    return {
-      ...transactions,
-    }
-  })
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
 
-  app.get('/:id', async (request) => {
-    const paramsScema = Zod.object({
-      id: Zod.string().uuid(),
-    })
+      return {
+        ...transactions,
+      }
+    },
+  )
 
-    const { id } = paramsScema.parse(request.params)
-
-    const transaction = await knex('transactions').where('id', id).first()
-
-    return {
-      ...transaction,
-    }
-  })
-
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      .sum('amount', {
-        as: 'ammount',
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkIdExist],
+    },
+    async (request) => {
+      const paramsScema = Zod.object({
+        id: Zod.string().uuid(),
       })
-      .first()
 
-    return {
-      ...summary,
-    }
-  })
+      const { id } = paramsScema.parse(request.params)
+      const sessionId = request.cookies.sessionId
+
+      const transaction = await knex('transactions')
+        .where({
+          session_id: sessionId,
+          id,
+        })
+        .first()
+
+      return {
+        ...transaction,
+      }
+    },
+  )
+
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkIdExist],
+    },
+    async (request) => {
+      const sessionId = request.cookies.sessionId
+
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', {
+          as: 'ammount',
+        })
+        .first()
+
+      return {
+        ...summary,
+      }
+    },
+  )
 }
 
 // cookies -> formas da gente manter contexto entre requisições
